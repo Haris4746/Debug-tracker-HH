8a4b0e2f8ed2f05d116b2db16783fb105abaf71d
Haris4746 <33449623+Haris4746@users.noreply.github.com>
2018-01-23 03:56:30 +0000

    Second form design
    Fixed a bug causing elements to display out of line.

---
+++ Form2.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace debugger
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.oldreports = new System.Windows.Forms.Button();
+            this.reportnew = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.BackColor = System.Drawing.Color.White;
+            this.label.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(73, 46);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(70, 22);
+            this.label.TabIndex = 0;
+            this.label.Text = "name";
+            // 
+            // oldreports
+            // 
+            this.oldreports.BackColor = System.Drawing.Color.Black;
+            this.oldreports.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.oldreports.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.oldreports.ForeColor = System.Drawing.Color.White;
+            this.oldreports.Location = new System.Drawing.Point(172, 150);
+            this.oldreports.Name = "oldreports";
+            this.oldreports.Size = new System.Drawing.Size(200, 37);
+            this.oldreports.TabIndex = 1;
+            this.oldreports.Text = "Reports";
+            this.oldreports.UseVisualStyleBackColor = false;
+            this.oldreports.Click += new System.EventHandler(this.oldreports_Click);
+            // 
+            // reportnew
+            // 
+            this.reportnew.BackColor = System.Drawing.SystemColors.Desktop;
+            this.reportnew.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reportnew.ForeColor = System.Drawing.Color.White;
+            this.reportnew.Location = new System.Drawing.Point(172, 193);
+            this.reportnew.Name = "reportnew";
+            this.reportnew.Size = new System.Drawing.Size(200, 40);
+            this.reportnew.TabIndex = 2;
+            this.reportnew.Text = "Create report";
+            this.reportnew.UseVisualStyleBackColor = false;
+            this.reportnew.Click += new System.EventHandler(this.reportnew_Click);
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(-1, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Logout";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(172, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 45);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Reported ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "User:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(165, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(230, 40);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "DASHBOARD";
+            // 
+            // Form2
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(548, 286);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.reportnew);
+            this.Controls.Add(this.oldreports);
+            this.Controls.Add(this.label);
+            this.HelpButton = true;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DEBUGGER";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button oldreports;
+        private System.Windows.Forms.Button reportnew;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace debugger
+{
+   
+    public partial class Form2 : Form
+    {
+        public string name;
+        public string id;
+
+
+        public void set_name(string name)
+        {
+            this.name = name;
+        }
+
+        public string get_name()
+        {
+            return this.name;
+                
+        }
+
+        public void set_id(string id)
+        {
+            this.id = id;
+        }
+
+        public string get_id()
+        {
+            return this.id;
+
+        }
+
+
+        public Form2(string name, string id)
+        {
+          
+           
+            InitializeComponent();
+
+             set_name(name);
+             set_id(id);
+            
+            label.Text = name;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void oldreports_Click(object sender, EventArgs e)
+        {
+            OldReport fm = new OldReport(get_name(), get_id());
+
+            this.Hide();
+            fm.Show();
+        }
+
+        public void reportnew_Click(object sender, EventArgs e)
+        {
+            newReport ur = new newReport(get_name(),get_id());
+          
+            this.Hide();
+            ur.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DEBUGGER f1 = new DEBUGGER();
+            this.Hide();
+            f1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 fm = new Form3(get_name(), get_id());
+
+            this.Hide();
+            fm.Show();
+        }
+    }
+}
